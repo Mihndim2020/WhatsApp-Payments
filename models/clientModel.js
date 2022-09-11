b84f62b70f0f375e58a80a9141a49d7cfd5483ed
Mihndim2020 <jelsieprisca2016@gmail.com>
2022-09-11 18:19:46 -0400

    Update client model

--- models/clientModel.js
+++ models/clientModel.js
@@ -6,10 +6,9 @@ const ClientSchema = new Schema(
   {
     telephone_number: {type: String, required: true, minlength: 9, maxLength: 13},
     merchand: { type: Schema.Types.ObjectId, ref: 'Merchand', required: true },
-    customer_experience: { type: Schema.Types.ObjectId, ref: 'CustomerExperience', required: true },
-    app_experience: { type: Schema.Types.ObjectId, ref: 'AppExperience', required: true },
     favorite_list: [{ type: String }],
-    transaction: { type: Schema.Types.ObjectId, ref: 'Transaction', required: true },
+    transaction_reference: {type: String, minlength: 2, maxLength: 100},
+    transaction_amount: {type: Number, required: true},
   }
 );
 
