00941bcc499b9b742b7e08dff40ecbe5bd8396a9
Mihndim2020 <jelsieprisca2016@gmail.com>
2022-09-26 07:47:56 -0400

    Update merchant schema

--- models/merchandModel.js
+++ models/merchandModel.js
@@ -6,6 +6,8 @@ const MerchandSchema = new Schema(
   {
     merchand_code: {type: String, required: true, maxLength: 100},
     merchand_token: {type: String, required: true, maxLength: 100},
+    merchand_private_token: {type: String, required: true, maxLength: 1000},
+    registered: {type: Boolean},
     merchand_name: {type: String, required: true, maxLength: 100},
     business_name: {type: String, required: true, maxLength: 100},
     telephone_number: {type: String, required: true, maxLength: 100},
